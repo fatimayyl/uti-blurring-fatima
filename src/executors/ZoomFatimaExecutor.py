19766cf749f8bab0f5d21849f248b11fe441d181
fatimayyl <yaylifatima@gmail.com>
2025-07-15 16:13:30 +0300

    Update ZoomFatimaExecutor.py

--- src/executors/ZoomFatimaExecutor.py
+++ src/executors/ZoomFatimaExecutor.py
@@ -1,5 +1,5 @@
 """
-    It is one of the preprocessing components in which the image is rotated.
+    It is one of the preprocessing components in which the image is zoomed.
 """
 
 import os
@@ -13,30 +13,65 @@ from sdks.novavision.src.base.component import Component
 from sdks.novavision.src.helper.executor import Executor
 from components.ZoomFatima.src.utils.response import build_response
 from components.ZoomFatima.src.models.PackageModel import PackageModel
+from components.ZoomFatima.src.utils.response import build_response_zoom
+
 
 
 class ZoomFatimaExecutor(Component):
     def __init__(self, request, bootstrap):
         super().__init__(request, bootstrap)
         self.request.model = PackageModel(**(self.request.data))
-        self.rotation_degree = self.request.get_param("Degree")
-        self.keep_side = self.request.get_param("KeepSide")
-        self.image = self.request.get_param("inputImage")
+
+
+        self.zoom_factor = self.request.get_param("ZoomFactor")
+        self.imageOne = self.request.get_param("inputImageOne")
+        self.imageTwo = self.request.get_param("inputImageTwo")
 
     @staticmethod
     def bootstrap(config: dict) -> dict:
         return {}
 
-    def zoom(self, img):
-        cropped = img[50:180, 100:300]  # 130x200
-        zoomed = cv2.resize(cropped, (img.shape[1], img.shape[0]), interpolation=cv2.INTER_LINEAR)
+    def zoom(self, img, zoom_factor=1.2):
+        """
+        Zooms into the center of the image by a zoom factor.
+        zoom_factor > 1 → Zoom in
+        zoom_factor < 1 → Zoom out
+        """
+
+        if zoom_factor == 1.0:
+            return img  # No zoom needed
+
+        height, width = img.shape[:2]
+
+        # Yeni alanın boyutları
+        new_width = int(width / zoom_factor)
+        new_height = int(height / zoom_factor)
+
+        # Merkeze göre crop sınırlarını hesapla
+        x1 = (width - new_width) // 2
+        y1 = (height - new_height) // 2
+        x2 = x1 + new_width
+        y2 = y1 + new_height
+
+        # Merkezden kırp
+        cropped = img[y1:y2, x1:x2]
+
+        # Orijinal boyutlara döndür
+        zoomed = cv2.resize(cropped, (width, height), interpolation=cv2.INTER_LINEAR)
+
         return zoomed
 
     def run(self):
-        img = Image.get_frame(img=self.image, redis_db=self.redis_db)
-        img.value = self.rotation(img.value)
-        self.image = Image.set_frame(img=img, package_uID=self.uID, redis_db=self.redis_db)
-        packageModel = build_response(context=self)
+        img1 = Image.get_frame(img=self.imageOne, redis_db=self.redis_db)
+        img2 = Image.get_frame(img=self.imageTwo, redis_db=self.redis_db)
+
+        img1.value = self.zoom(img1.value, zoom_factor=self.zoom_factor)
+        img2.value = self.zoom(img2.value, zoom_factor=self.zoom_factor)
+
+        self.imageOne = Image.set_frame(img=img1, package_uID=self.uID, redis_db=self.redis_db)
+        self.imageTwo = Image.set_frame(img=img2, package_uID=self.uID, redis_db=self.redis_db)
+
+        packageModel = build_response_zoom(context=self)
         return packageModel
 
 
