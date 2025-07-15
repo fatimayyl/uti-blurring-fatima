d2c6d6e96bf2f74db80749be083968cbd26daab6
fatimayyl <yaylifatima@gmail.com>
2025-07-15 23:33:26 +0300

    Update response.py

--- src/utils/response.py
+++ src/utils/response.py
@@ -2,7 +2,7 @@
 from sdks.novavision.src.helper.package import PackageHelper
 from components.BlurringFatima.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, BlurringFatimaExecutorOutputs, BlurringFatimaExecutorResponse, BlurringFatimaExecutor, OutputImageOne, OutputImageTwo
 from components.BlurringFatima.src.models.PackageModel import ZoomFatimaExecutorOutputs, ZoomFatimaExecutorResponse, ZoomFatimaExecutor
-
+from components.BlurringFatima.src.models.PackageModel import GrayFatimaExecutorOutputs, GrayFatimaExecutorResponse, GrayFatimaExecutor
 
 
 def build_response(context):
@@ -24,6 +24,18 @@ def build_response_zoom(context):
     zoomFatimaExecutorResponse = ZoomFatimaExecutorResponse(outputs=zoomFatimaExecutorOutputs)
     zoomFatimaExecutor = ZoomFatimaExecutor(value=zoomFatimaExecutorResponse)
     configexecutor = ConfigExecutor(value=zoomFatimaExecutor)
+    packageConfigs = PackageConfigs(executor=configexecutor)
+    package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
+    packageModel = package.build_model(context)
+    return packageModel
+
+def build_response_gray(context):
+    outputImageOne = OutputImageOne(value=context.imageOne)
+    outputImageTwo = OutputImageTwo(value=context.imageTwo)
+    grayFatimaExecutorOutputs = GrayFatimaExecutorOutputs(outputImageOne=outputImageOne, outputImageTwo=outputImageTwo)
+    grayFatimaExecutorResponse = GrayFatimaExecutorResponse(outputs=grayFatimaExecutorOutputs)
+    grayFatimaExecutor = GrayFatimaExecutor(value=grayFatimaExecutorResponse)
+    configexecutor = ConfigExecutor(value=grayFatimaExecutor)
     packageConfigs = PackageConfigs(executor=configexecutor)
     package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
     packageModel = package.build_model(context)
