dbc2f3da56331b93c09e6508f92c34f03a12f9f5
fatimayyl <yaylifatima@gmail.com>
2025-07-15 22:23:21 +0300

    Update ZoomFatimaExecutor.py

--- src/executors/ZoomFatimaExecutor.py
+++ src/executors/ZoomFatimaExecutor.py
@@ -14,7 +14,6 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../'))
 from sdks.novavision.src.media.image import Image
 from sdks.novavision.src.base.component import Component
 from sdks.novavision.src.helper.executor import Executor
-from components.ZoomFatima.src.utils.response import build_response
 from components.ZoomFatima.src.models.PackageModel import PackageModel
 from components.ZoomFatima.src.utils.response import build_response_zoom
 
