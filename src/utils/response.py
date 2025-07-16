811ed012b8961b9504f498efe4079685139ce789
fatimayyl <yaylifatima@gmail.com>
2025-07-16 13:23:07 +0300

    Update response.py

--- src/utils/response.py
+++ src/utils/response.py
@@ -3,6 +3,7 @@ from sdks.novavision.src.helper.package import PackageHelper
 from components.BlurringFatima.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, BlurringFatimaExecutorOutputs, BlurringFatimaExecutorResponse, BlurringFatimaExecutor, OutputImageOne, OutputImageTwo
 from components.BlurringFatima.src.models.PackageModel import ZoomFatimaExecutorOutputs, ZoomFatimaExecutorResponse, ZoomFatimaExecutor
 from components.BlurringFatima.src.models.PackageModel import GrayFatimaExecutorOutputs, GrayFatimaExecutorResponse, GrayFatimaExecutor
+from components.BlurringFatima.src.models.PackageModel import CropFatimaExecutorOutputs, CropFatimaExecutorResponse, CropFatimaExecutor
 
 
 def build_response(context):
@@ -36,6 +37,18 @@ def build_response_gray(context):
     grayFatimaExecutorResponse = GrayFatimaExecutorResponse(outputs=grayFatimaExecutorOutputs)
     grayFatimaExecutor = GrayFatimaExecutor(value=grayFatimaExecutorResponse)
     configexecutor = ConfigExecutor(value=grayFatimaExecutor)
+    packageConfigs = PackageConfigs(executor=configexecutor)
+    package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
+    packageModel = package.build_model(context)
+    return packageModel
+
+def build_response_crop(context):
+    outputImageOne = OutputImageOne(value=context.imageOne)
+    outputImageTwo = OutputImageTwo(value=context.imageTwo)
+    cropFatimaExecutorOutputs = CropFatimaExecutorOutputs(outputImageOne=outputImageOne, outputImageTwo=outputImageTwo)
+    cropFatimaExecutorResponse = CropFatimaExecutorResponse(outputs=cropFatimaExecutorOutputs)
+    cropFatimaExecutor = CropFatimaExecutor(value=cropFatimaExecutorResponse)
+    configexecutor = ConfigExecutor(value=cropFatimaExecutor)
     packageConfigs = PackageConfigs(executor=configexecutor)
     package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
     packageModel = package.build_model(context)
