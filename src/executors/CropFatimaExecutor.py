aa9ce3f206f262191d8803eb5346982788f53f11
fatimayyl <yaylifatima@gmail.com>
2025-07-17 20:28:47 +0300

    fix: CropFatimaExecutor.py changed

--- src/executors/CropFatimaExecutor.py
+++ src/executors/CropFatimaExecutor.py
@@ -1,13 +1,14 @@
 import os
-import cv2
 import sys
+import cv2
+import numpy as np
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../'))
 
 from sdks.novavision.src.media.image import Image
 from sdks.novavision.src.base.component import Component
 from sdks.novavision.src.helper.executor import Executor
-from components.BlurringFatima.src.models.PackageModel import PackageModel
+from components.BlurringFatima.src.models.PackageModel import PackageModel, CropTopOption, CropBottomOption
 from components.BlurringFatima.src.utils.response import build_response_crop
 
 
@@ -16,58 +17,33 @@ class CropFatimaExecutor(Component):
         super().__init__(request, bootstrap)
         self.request.model = PackageModel(**self.request.data)
 
-        self.crop_mode = self.request.get_param("CropMode")
-        self.crop_pixels = self.request.get_param("CropPixels")
         self.imageOne = self.request.get_param("inputImageOne")
         self.imageTwo = self.request.get_param("inputImageTwo")
+        self.crop_mode = self.request.get_config("CropMode")
 
     @staticmethod
     def bootstrap(config: dict) -> dict:
         return {}
 
-    def crop(self, img, mode, pixels):
-        print(f"Cropping mode: {mode}, pixels: {pixels}, image size: {img.shape}")
-
+    def crop_image(self, img, mode):
         h, w = img.shape[:2]
-        p = int(pixels)
 
-        if mode == "CropTop":
-            return img[p:, :]
-        elif mode == "CropBottom":
-            return img[:h - p, :]
-        elif mode == "CropLeft":
-            return img[:, p:]
-        elif mode == "CropRight":
-            return img[:, :w - p]
+        if isinstance(mode, CropTopOption):
+            pixels = mode.config.value
+            return img[pixels:, :]
+        elif isinstance(mode, CropBottomOption):
+            pixels = mode.config.value
+            return img[:h - pixels, :]
         else:
-            print("Unknown crop mode, returning original image.")
+            print("Unknown crop mode. Returning original image.")
             return img
 
     def run(self):
-        print("crop_mode:", self.crop_mode)
-        print("crop_pixels (raw):", self.crop_pixels)
-        print("imageOne param:", self.imageOne)
-        print("imageTwo param:", self.imageTwo)
-
         img1 = Image.get_frame(img=self.imageOne, redis_db=self.redis_db)
         img2 = Image.get_frame(img=self.imageTwo, redis_db=self.redis_db)
 
-        # crop_pixels güvenli dönüşümü
-        if hasattr(self.crop_pixels, "value"):
-            try:
-                crop_pixels = int(self.crop_pixels.value)
-            except Exception as e:
-                print("Error converting crop_pixels.value to int:", e)
-                crop_pixels = 50
-        else:
-            try:
-                crop_pixels = int(self.crop_pixels)
-            except Exception as e:
-                print("Error converting crop_pixels to int:", e)
-                crop_pixels = 50
-
-        cropped1 = self.crop(img1.value, self.crop_mode, crop_pixels)
-        cropped2 = self.crop(img2.value, self.crop_mode, crop_pixels)
+        cropped1 = self.crop_image(img1.value, self.crop_mode.value)
+        cropped2 = self.crop_image(img2.value, self.crop_mode.value)
 
         print("Cropped1 shape:", cropped1.shape)
         print("Cropped2 shape:", cropped2.shape)
@@ -81,15 +57,9 @@ class CropFatimaExecutor(Component):
         self.imageOne = Image.set_frame(
             img=img1_cropped, package_uID=self.uID, redis_db=self.redis_db
         )
-        print("Set frame imageOne:", self.imageOne)
-
         self.imageTwo = Image.set_frame(
             img=img2_cropped, package_uID=self.uID, redis_db=self.redis_db
         )
-        print("Set frame imageTwo:", self.imageTwo)
-
-        print("imageOne is None?", self.imageOne is None)
-        print("imageTwo is None?", self.imageTwo is None)
 
         return build_response_crop(context=self)
 
