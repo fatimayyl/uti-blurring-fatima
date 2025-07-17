942bc6f8c0f1ce2116436772e74af0fb6d0635c8
fatimayyl <yaylifatima@gmail.com>
2025-07-17 13:31:52 +0300

    fix: CropFatimaExecutor.py changed

--- src/executors/CropFatimaExecutor.py
+++ src/executors/CropFatimaExecutor.py
@@ -11,7 +11,6 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../'))
 from sdks.novavision.src.media.image import Image
 from sdks.novavision.src.base.component import Component
 from sdks.novavision.src.helper.executor import Executor
-from components.BlurringFatima.src.utils.response import build_response_crop
 from components.BlurringFatima.src.models.PackageModel import PackageModel
 
 
@@ -20,15 +19,26 @@ class CropFatimaExecutor(Component):
         super().__init__(request, bootstrap)
         self.request.model = PackageModel(**self.request.data)
 
+        # Crop config verilerini al
         crop_mode_obj = self.request.get_param("CropMode")
-        self.crop_mode = crop_mode_obj.get("value") if isinstance(crop_mode_obj, dict) else crop_mode_obj
 
-        self.crop_top_pixels = crop_mode_obj.get("config", {}).get("CropTopPixels", {}).get("value") \
-            if self.crop_mode == "CropTop" else None
+        if isinstance(crop_mode_obj, str):
+            self.crop_mode = crop_mode_obj
+            self.crop_top_pixels = self.request.get_param("CropTopPixels")
+            self.crop_bottom_pixels = self.request.get_param("CropBottomPixels")
 
-        self.crop_bottom_pixels = crop_mode_obj.get("config", {}).get("CropBottomPixels", {}).get("value") \
-            if self.crop_mode == "CropBottom" else None
+        elif isinstance(crop_mode_obj, dict):
+            self.crop_mode = crop_mode_obj.get("value")
+            config = crop_mode_obj.get("config", {})
+            self.crop_top_pixels = config.get("CropTopPixels", {}).get("value") if self.crop_mode == "CropTop" else None
+            self.crop_bottom_pixels = config.get("CropBottomPixels", {}).get("value") if self.crop_mode == "CropBottom" else None
 
+        else:
+            self.crop_mode = None
+            self.crop_top_pixels = None
+            self.crop_bottom_pixels = None
+
+        # Giriş görüntüleri
         self.imageOne = self.request.get_param("inputImageOne")
         self.imageTwo = self.request.get_param("inputImageTwo")
 
@@ -39,36 +49,24 @@ class CropFatimaExecutor(Component):
     def crop(self, img, crop_mode, top_px, bottom_px):
         height, width = img.shape[:2]
         if crop_mode == "CropTop" and top_px:
-            print(f"Cropping TOP: {top_px}px")
+            print(f"🟡 Cropping from top: {top_px}px")
             return img[int(top_px):, :]
         elif crop_mode == "CropBottom" and bottom_px:
-            print(f"Cropping BOTTOM: {bottom_px}px")
+            print(f"🟡 Cropping from bottom: {bottom_px}px")
             return img[:height - int(bottom_px), :]
-        print("NO CROP applied")
+        print("⚠️ No crop applied")
         return img
 
     def run(self):
         img1 = Image.get_frame(img=self.imageOne, redis_db=self.redis_db)
         img2 = Image.get_frame(img=self.imageTwo, redis_db=self.redis_db)
 
-        top_px = (
-            self.crop_top_pixels.value
-            if self.crop_mode == "CropTop" and hasattr(self.crop_top_pixels, "value")
-            else None
-        )
+        print("🔧 CROP MODE:", self.crop_mode)
+        print("🔧 TOP PIXELS:", self.crop_top_pixels)
+        print("🔧 BOTTOM PIXELS:", self.crop_bottom_pixels)
 
-        bottom_px = (
-            self.crop_bottom_pixels.value
-            if self.crop_mode == "CropBottom" and hasattr(self.crop_bottom_pixels, "value")
-            else None
-        )
-
-        print("MODE:", self.crop_mode)
-        print("TOP:", top_px)
-        print("BOTTOM:", bottom_px)
-
-        img1.value = self.crop(img1.value, self.crop_mode, top_px, bottom_px)
-        img2.value = self.crop(img2.value, self.crop_mode, top_px, bottom_px)
+        img1.value = self.crop(img1.value, self.crop_mode, self.crop_top_pixels, self.crop_bottom_pixels)
+        img2.value = self.crop(img2.value, self.crop_mode, self.crop_top_pixels, self.crop_bottom_pixels)
 
         self.imageOne = Image.set_frame(img=img1, package_uID=self.uID, redis_db=self.redis_db)
         self.imageTwo = Image.set_frame(img=img2, package_uID=self.uID, redis_db=self.redis_db)
